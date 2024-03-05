5248a8c6348849a3ad44d41c6b62f458a3b54ceb
Bernardo <bernardpena@gmail.com>
2024-03-05 01:26:46 -0300

    Agregando segundo ejercicio

--- assets/js/imagen.js
+++ assets/js/imagen.js
@@ -2,19 +2,17 @@ let boderactivo = 1
 
 function resaltar(){
     border = document.querySelector("#imagen");
+    
     if (boderactivo == 1){
         border.style.borderStyle = "solid"
         border.style.borderColor = "#E74C3C";
         border.style.borderWidth = "2px";
         border.style.borderRadius = "50%";
         boderactivo = 0
-        
-
     } else {
         boderactivo = 1
         border.style.borderStyle = "none"
         border.style.borderRadius = "0%";
-
     }
     
 }
