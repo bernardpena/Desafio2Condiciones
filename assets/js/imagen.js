93a6bedda51100398c28391d8124cd3e815f6db4
Bernardo <bernardpena@gmail.com>
2024-03-07 15:37:30 -0300

    Tercer desafio Agregado

--- assets/js/imagen.js
+++ assets/js/imagen.js
@@ -1,3 +1,4 @@
+// Primer Desafió!!
 let boderactivo = 1;
 
 function resaltar() {
@@ -17,9 +18,9 @@ function resaltar() {
 }
 
 function verificar() {
-  var ingreso1 = document.getElementById("ingreso1").value;
-  var ingreso2 = document.getElementById("ingreso2").value;
-  var ingreso3 = document.getElementById("ingreso3").value;
+  let ingreso1 = document.getElementById("ingreso1").value;
+  let ingreso2 = document.getElementById("ingreso2").value;
+  let ingreso3 = document.getElementById("ingreso3").value;
 
   maximo = Number(ingreso1) + Number(ingreso2) + Number(ingreso3);
 
@@ -29,5 +30,69 @@ function verificar() {
   } else {
     texto = document.querySelector("span");
     texto.innerHTML = "Llevas demasiados stickers";
+  }
+}
+
+// segundo Desafió!!
+let listaOpciones = new Array(
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10"
+);
+window.onload = listNumero1();
+window.onload = listNumero2();
+window.onload = listNumero3();
+
+function listNumero1() {
+  let lista = document.getElementById("opciones1");
+  for (i = 0; i < listaOpciones.length; i++) {
+    lista.options[i] = new Option(listaOpciones[i]);
+  }
+}
+
+function listNumero2() {
+  let lista = document.getElementById("opciones2");
+  for (i = 0; i < listaOpciones.length; i++) {
+    lista.options[i] = new Option(listaOpciones[i]);
+  }
+}
+
+function listNumero3() {
+  let lista = document.getElementById("opciones3");
+  for (i = 0; i < listaOpciones.length; i++) {
+    lista.options[i] = new Option(listaOpciones[i]);
+  }
+}
+
+//Tercer Desafió!!
+function password() {
+  let acceso1 = document.getElementById("opciones1");
+  let pass1 = acceso1.value;
+
+  let acceso2 = document.getElementById("opciones2");
+  let pass2 = acceso2.value;
+
+  let acceso3 = document.getElementById("opciones3");
+  let pass3 = acceso3.value;
+
+  let llave = pass1 + pass2 + pass3;
+
+  if (llave == 911) {
+    texto = document.getElementById("correcto");
+    texto.innerHTML = "Password 1 correcto";
+  } else if (llave == 714) {
+    texto = document.getElementById("correcto");
+    texto.innerHTML = "Password 2 correcto";
+  } else {
+    texto = document.getElementById("correcto");
+    texto.innerHTML = "Password incorrecto";
   }
 }
